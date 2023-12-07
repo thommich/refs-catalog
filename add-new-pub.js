df4582b1a95e2c87f28586100d6464bfa140459b
Thomás Michelena Santos <39322621+thommich@users.noreply.github.com>
2023-12-07 02:15:26 -0300

    Update add-new-pub.js

--- add-new-pub.js
+++ add-new-pub.js
@@ -4,8 +4,9 @@ const publication = require('./publication.json');
 
 const user = eventPayload.sender.login;
 const [pub_title, year_of_pub, tldr, pub_link] = Object.values(publication);
+console.log(pub_title)
 
 const content = `| ${pub_title} | ${year_of_pub} | ${tldr} | ${pub_link} | [@${user}](https://github.com/${user}) |`;
 console.log(content)
 
-fs.appendFileSync('refs-catalog/pubs.md', content);
+fs.appendFileSync('./refs-catalog/pubs.md', content);
