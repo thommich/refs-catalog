6dad53e790b3d4914aae276f8e3c5e7a7854b02f
Thomás Michelena Santos <39322621+thommich@users.noreply.github.com>
2024-02-07 01:19:49 -0300

    Create add-new-pub.py

---
+++ add-new-pub.py
@@ -0,0 +1,30 @@
+import os
+import json
+
+ev_payload = os.environ['GITHUB_EVENT_PATH']
+publication = json.loads('./publication.json')
+
+user = ev_payload.sender.login
+pub_title = publication['title']
+year_of_pub = publication['year_of_pub']
+tldr = publication['tldr']
+pub_link = publication['pub_link']
+
+content = f`\n| {pub_title} | {year_of_pub} | {tldr} | {pub_link} | [@{user}](https://github.com/{user}) |`
+pubs_file_path = os.path.join(_os.getcwd(), '/refs-catalog/docs/pubs.md');
+
+with open(pubs_file_path, "a") as file_pub:
+  file_pub.write(content)
+
+
+# const fs = require('fs');
+# const path = require('path');
+# const eventPayload = require(process.env.GITHUB_EVENT_PATH);
+# const publication = require('./publication.json');
+
+# const user = eventPayload.sender.login;
+# const [pub_title, year_of_pub, tldr, pub_link] = Object.values(publication);
+# const content = `\n| ${pub_title} | ${year_of_pub} | ${tldr} | ${pub_link} | [@${user}](https://github.com/${user}) |`;
+# const pubsFilePath = path.join(__dirname, '/refs-catalog/docs/pubs.md');
+
+# fs.appendFileSync(pubsFilePath, content);
