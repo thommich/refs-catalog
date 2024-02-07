52ce95637c5218a6fa551f7e1cc9f01e4558d06c
Thomás Michelena Santos <39322621+thommich@users.noreply.github.com>
2024-02-07 01:32:18 -0300

    Update add-new-pub.py

--- add-new-pub.py
+++ add-new-pub.py
@@ -3,6 +3,7 @@ import json
 
 ev_payload = os.environ['GITHUB_EVENT_PATH']
 publication = json.loads('./publication.json')
+print(publication)
 
 user = ev_payload.sender.login
 pub_title = publication['title']
@@ -10,11 +11,14 @@ year_of_pub = publication['year_of_pub']
 tldr = publication['tldr']
 pub_link = publication['pub_link']
 
-content = f`\n| {pub_title} | {year_of_pub} | {tldr} | {pub_link} | [@{user}](https://github.com/{user}) |`
-pubs_file_path = os.path.join(_os.getcwd(), '/refs-catalog/docs/pubs.md');
+content = f`\\n| {pub_title} | {year_of_pub} | {tldr} | {pub_link} | [@{user}](https://github.com/{user}) |`
+pubs_file_path = os.path.join(os.getcwd(), '/refs-catalog/docs/pubs.md');
 
 with open(pubs_file_path, "a") as file_pub:
   file_pub.write(content)
+
+os.remove('./publication.json')
+
 
 
 # const fs = require('fs');
